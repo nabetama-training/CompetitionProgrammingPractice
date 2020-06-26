a0148d0046ed0ad2ce6d2b694b41f0df9e9312c4
nabetama <mao.nabeta@gmail.com>
2020-06-27 01:51:15 +0900

    https://atcoder.jp/contests/abs/tasks/practice_1

---
+++ src/atcoder/practice_1.py
@@ -0,0 +1,6 @@
+# https://atcoder.jp/contests/abs/tasks/practice_1
+def resolve():
+    a = int(input())
+    b, c = list(map(int, input().split()))
+    d = input()
+    print("{} {}".format(a + b + c, d))
