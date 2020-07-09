98be85e8a7c8cdf1240f442493cf745da3c71c35
nabetama <mao.nabeta@gmail.com>
2020-07-09 23:53:53 +0900

    fixed ITP1_5_D http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_5_D

--- src/AOJ/ITP1_5_D.py
+++ src/AOJ/ITP1_5_D.py
@@ -1,12 +1,6 @@
 def resolve():
     n = int(input())
-
     for i in range(1, n + 1):
-        if i % 3 == 0 and i % 10 == 3:
+        if i % 3 == 0 or "3" in str(i):
             print(" {}".format(i), end="")
-        elif i % 3 == 0:
-            print(" {}".format(i), end="")
-        elif i % 10 == 3:
-            print(" {}".format(i), end="")
-        if i == n:
-            print(" ")
+    print()
