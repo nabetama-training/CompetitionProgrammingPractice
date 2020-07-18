f524f779d2f6459832dd0c1a49f3795cb2c4f039
nabetama <mao.nabeta@gmail.com>
2020-07-18 15:45:02 +0900

    format?

--- src/AOJ/ITP1_10_B.py
+++ src/AOJ/ITP1_10_B.py
@@ -1,14 +1,12 @@
 import math
-from decimal import Decimal, ROUND_HALF_UP
 
 
 def resolve():
-    a, b, C = map(int, input().split())
+    a, b, C = map(float, input().split())
     x = math.radians(C)
 
     h = b * math.sin(x)
-    S = Decimal((a * h) / 2).quantize(Decimal('0.00000001'), rounding=ROUND_HALF_UP)
-    c = Decimal(math.sqrt(a ** 2 + b ** 2 - 2 * a * b * math.cos(x))).quantize(Decimal('0.00000001'),
-                                                                               rounding=ROUND_HALF_UP)
-    L = Decimal(a + b + c).quantize(Decimal('0.00000001'), rounding=ROUND_HALF_UP)
+    S = "{0:.8f}".format((a * h) / 2)
+    c = math.sqrt(a ** 2 + b ** 2 - 2 * a * b * math.cos(x))
+    L = "{0:.8f}".format(a + b + c)
     print(S, L, h, sep="\n")
